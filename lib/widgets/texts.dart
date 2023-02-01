63f8b07e77936720267f988e0f2d6ae76500f3b7
Onkar-K-Mane <onkarkmane1501@gmail.com>
2023-02-01 21:18:09 +0530

    help Screen Done

--- lib/widgets/texts.dart
+++ lib/widgets/texts.dart
@@ -12,7 +12,7 @@ class TextBottomUI extends StatelessWidget {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Container(
-          height: MediaQuery.of(context).size.height / 2.6,
+          height: MediaQuery.of(context).size.height / 4,
           width: MediaQuery.of(context).size.width,
           padding: EdgeInsets.only(left: 4, right: 4, top: 60),
           child: Center(
@@ -25,10 +25,27 @@ class TextBottomUI extends StatelessWidget {
                   fontFamily: 'OnBoardingFont',
                   letterSpacing: 2,
                   wordSpacing: 3,
-                  height: 1.5),
+                  height: 1.3),
               textAlign: TextAlign.center,
             ),
           )),
+    );
+  }
+}
+
+class TextList extends StatelessWidget {
+  TextList(this.text, {super.key});
+  String? text;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text!,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 25,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'ButtonFont',
+      ),
     );
   }
 }
